f49ba3c39079e0e71796349c4996b6c03ca5587a
yasiasz <yurizapata22@gmail.com>
2016-08-10 09:46:44 -0500

    first

---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  get 'static_pages/home'
+  get 'static_pages/hola'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
